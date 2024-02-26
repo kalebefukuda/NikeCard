ad4111c5262f748854056996cb76a6a6ec9fe2af
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-02-25 23:42:05 -0300

    project Nike Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Tenis from './assets/tenis.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='w-full h-full overflow-hidden '>
+      
+        <div className=' bg-slate-300 flex justify-center items-center w-full h-screen p-3 font-poppins text-start'>
+          <div className='bg-white h-[540px] w-full max-w-4xl shadow-xl flex flex-col items-center justify-center md:flex-row'>
+            <div className='w-full h-full md:w-1/2 flex items-center justify-center flex-col' >
+                <div className='w-full'>
+                    <h1 className='hidden md:block text-sm ml-4 font-bold mt-4'>kalebe fukuda</h1>
+                </div>
+                <div className='w-full h-full flex items-center justify-center' >
+                    <div  className='mb-2'>
+                      <h1 className='text-5xl font-bold mb-2 select-none'>Lançamento <br />
+                      <p className='animate-floatRight'>Nake Air.</p>
+                      </h1>
+                      <p className='mb-1 animate-opacityPulse'> O Tênis do futuro</p>
+                      <p className='mb-7'> O futuro a um passo do seu alcance</p>
+                      <a href="#" className='font-bold bg-zinc-800 text-white py-2 px-5 border-2 border-zinc-800 duration-200 hover:bg-white hover:text-zinc-800' > QUERO CONHECER!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-full h-full md:w-1/2 flex items-center justify-center bg-bg_tenis bg-cover bg-no-repeat bg-center'>
+                <img src={Tenis} alt="Tenis Nike Air" className='max-w-lg w-11/12 md:w-full object-cover z-30 duration-300 hover:-rotate-6 hover:scale-110 md:hover:scale-125' />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "poppins": ["Poppins"]
+      },
+      backgroundImage:{
+        "bg_tenis": "url('/src/assets/bg.png')"
+      },
+      keyframes:{
+        floatRight:{
+          "0%": {transform : "translatex(0px)"},
+          "50%": {transform : "translatex(8px)"},
+          "100%": {transform : "translatex(0px)"},
+
+        },
+        opacityPulse:{
+          "0%": {opacity: 1},
+          "50%": {opacity: 0.2},
+          "100%": {opacity: 1},
+        }
+      },
+      animation:{
+        floatRight:"floatRight 2.5s infinite",
+        opacityPulse: "opacityPulse 2s infinite",
+
+      }
+    },
   },
   plugins: [],
 }
